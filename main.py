b8025bfe447467b3104b3ad0d09ed284111f9962
annavikarchuk <kalinina_anna_spb@mail.ru>
2024-12-06 21:00:35 +0300

    tuple theory

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-# This is a sample Python script.
+# В кортеж список нельзя ни добавить, ни удалить. Но если его элемент список,
+# то в этот список можно добавить/ удалить
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# a = (1, ["one", "two"])
+# b = () #пустой кортеж
+# c = tuple() # с помощью готовой функции
+# d = tuple('Hello guy') # разбивает строку побуквенно
+# a[1][0] = 3
+
+a = (1, 2, 3, "poo", [1, 5, 6], True) #!!!!кортеж меньше весит
+b = [1, 2, 3, "poo", [1, 5, 6], True]
+
+#a.count(2)  # выводит кол-во раз сколько встречвается значение в кортеже
+#index = a.index("poo") # работает с числами, строками - выводит порядковый номер
+
+# size_a= a.__sizeof__()
+# size_b= b.__sizeof__()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# print(3 in a) # наличие элемента в кортеже/ списке
+# print(0 not in a)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    # print_hi('PyCharm')
-    number = 100 #integer
-    float_number = 100.5 #float
-    text = 'Цитата: \'программирование\''
-    print(text)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+numbers = (1, 2, 3, 2, 1, 2)
+print(numbers.count(2)) # 3 (число 2 встречается три раза)
+print(numbers.index(3)) # 2 (индекс первого вхождения 3)
